8b4fa9282c1e3b96042e8b11cf3f8b6afc037490
Grzegorz Michlicki <grzegorz@michlicki.pl>
2020-08-02 12:21:13 +0200

    Simplified index.html building. Bundling everything instead of using CDN libs.

--- rollup.config.js
+++ rollup.config.js
@@ -6,40 +6,19 @@ import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
 import { template } from './template';
 
-const deps = [
-  {
-    name: 'pixi.js',
-    global: 'PIXI',
-    path: '/dist/pixi.min.js',
-  }, {
-    name: '@ash.ts/ash',
-    global: 'ASH',
-    path: '/dist/ash.min.js',
-  },
-];
-
-const globals = deps.reduce((glob, dep) => {
-  glob[dep.name] = dep.global;
-  return glob;
-}, {});
-
-const external = Object.keys(globals);
-
 export default {
-  external,
   input: 'src/index.ts',
   output: {
-    globals,
     file: 'dist/asteroids.js',
     format: 'iife',
     sourcemap: false,
   },
   plugins: [
-    resolve({ preferBuiltins: true }),
+    resolve({ preferBuiltins: false }),
     commonjs(),
     typescript(),
     terser({ output: { comments: false } }),
-    html({ template: template(deps) }),
+    html({ template }),
     url({ limit: 0, include: ['**/*.mp3', '**/*.ogg'] }),
   ],
 };
